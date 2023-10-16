0440c34a3ddb3c54d9f73ee336533681e6762da9
offbeat-dev <offbeat.it.all@gmail.com>
2023-10-16 18:46:48 -0500

    :wrench: - fix build paths

--- vite.config.js
+++ vite.config.js
@@ -10,19 +10,16 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         "01-scrolltrigger-scrub": resolve(
           __dirname,
-          "pages/01-scrolltrigger-scrub/index.html"
+          "/01-scrolltrigger-scrub/index.html"
         ),
         "02-threejs-polygons": resolve(
           __dirname,
-          "pages/02-threejs-polygons/index.html"
+          "/02-threejs-polygons/index.html"
         ),
-        "03-react-starter": resolve(
-          __dirname,
-          "pages/03-react-starter/index.html"
-        ),
+        "03-react-starter": resolve(__dirname, "/03-react-starter/index.html"),
         "04-react-unstated-counter": resolve(
           __dirname,
-          "pages/04-react-unstated-counter/index.html"
+          "/04-react-unstated-counter/index.html"
         ),
       },
     },
