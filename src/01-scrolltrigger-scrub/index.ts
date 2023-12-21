c7cb2c6baaa102f3e6fc297c1f7b5376ad33964a
offbeat-dev <offbeat.it.all@gmail.com>
2023-12-21 11:36:36 -0500

    :wrench: - add lenis to scrolltrigger example

--- src/01-scrolltrigger-scrub/index.ts
+++ src/01-scrolltrigger-scrub/index.ts
@@ -1,24 +1,30 @@
+import Lenis from "@studio-freight/lenis";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import "../styles/globals.scss";
 import "./styles.css";
 
-gsap.registerPlugin(ScrollTrigger);
-ScrollTrigger.config({ ignoreMobileResize: true });
 const content = document.querySelector(".content");
 const mainSection = document.querySelector(".main-section");
-
+const lenis = new Lenis({
+  lerp: 0.5,
+});
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: mainSection,
-    start: "top top",
-    end: "+=100%",
-    scrub: 5,
+    start: "center center",
+    end: "center start",
+    scrub: 2,
   },
 });
 
 tl.to(content, {
-  yPercent: -100,
+  yPercent: -10,
   ease: "none",
 });
 
